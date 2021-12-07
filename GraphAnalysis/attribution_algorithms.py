eba3b6d682e747c94053554e901cd4146e303d33
HolmlundVD <71910342+HolmlundVD@users.noreply.github.com>
2021-12-06 18:26:01 -0600

    Update attribution_algorithms.py
    Added adversarial to attribution algorithms

--- GraphAnalysis/attribution_algorithms.py
+++ GraphAnalysis/attribution_algorithms.py
@@ -4,11 +4,12 @@ import json
 import numpy as np
 import pickle
 alphabet="!@#$englishalphabet"
+threshold=0
 def adversarial_with_markov(training_file,testing_file,context_len):
     all_emissions=dict()
     english=""
     with open(training_file) as text:
-            train_data=json.load(text)
+        train_data=json.load(text)
     
     model = hmm.MultinomialHMM(n_components=4)
     model.algorithm='viterbi'
@@ -23,27 +24,36 @@ def adversarial_with_markov(training_file,testing_file,context_len):
     Probs=np.random.rand(4,128)
     row_sums = Probs.sum(axis=1)
     Probs = Probs / row_sums[:, np.newaxis]
-    model.emissionprob_= Probs
+    model.emissionprob_=Probs
    
-    print("to start")
-    for author,comments in train_data.items(): 
-       
-        characters=[[ord(i)] for i in hypothesis_testing.deEmojify(comments[1])]
-        char_ar=np.asarray(characters)
-        uno=np.ones(shape=(4,128),dtype=int)
-        print(model.emissionprob_.shape==(model.n_components,model.n_features))
-        
-        model.fit(uno)       
+    
+    for author,comments in train_data.items():      
+        characters=[[ord(i)] for i in hypothesis_testing.deEmojify(comments[1])]  
+        model.fit(characters)
         all_emissions[author]=model.emissionprob_
         english+=comments
     english_chars=[[i] for i in hypothesis_testing.deEmojify(english)] 
     model.fit(english_chars)
     all_emissions[alphabet]=model.emissionprob_
-     
-   
-
+    total_matched=0
+    total_tested=0
+    for tweet in test_data:
+        text=hypothesis_testing.deEmojify(tweet)
+        frequencies=[text.count(ord(i)) for i in range(128)]
+        best_matched_score=sys.minint
+        best_matched_author=""
+        for author in all_emissions.keys():
+            author_sum=0
+            for i in range(4):
+                for j in range(128):
+                    author_sum+=frequencies[j]*(all_emissions[author][i][j]-all_emissions[alphabet][i][j])/all_emissions[author][i][j]
+            if author_sum>best_matched_score:
+                best_matched_score=author_sum
+                best_matched_author=author
+        total_tested+=1
+        if best_matched_author==author:
+            total_matched+=1
     
-
 if __name__=="__main__":
     adversarial_with_markov("bitcoin//training80.txt","bitcoin//testing80.txt",3)
 
